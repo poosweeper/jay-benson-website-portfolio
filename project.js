734bb75fbb5fa17b6c3599a81a665401324cdbe1
poosweeper <jaybenson456@gmail.com>
2024-01-17 11:07:58 +0300

    No major changes, just minor to JS

--- project.js
+++ project.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const image = document.querySelector('.image_about_one img');
   
     image.addEventListener('mouseenter', function () {
-      image.style.transition = 'transform 10s ease'; // Adjust the duration as needed
+      image.style.transition = 'transform 12s ease'; // Adjust the duration as needed
       image.style.transform = 'translateY(500px)';
     });
   
